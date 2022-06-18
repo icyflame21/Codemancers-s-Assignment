a07441e53dfce89b25d452d0c11b0646b0b90cae
biswa-ranjan-subudhi <biswaranjan.cuh@gmail.com>
2022-06-18 23:39:19 +0530

    Assignment Done

--- src/Components/UserInput.jsx
+++ src/Components/UserInput.jsx
@@ -7,6 +7,7 @@ export function UserInput() {
   const [toggleGifBox, setToggleGifBox] = useState(false);
   const [selectedGifShow, setSelectedGifShow] = useState();
   const [writtenPost, setWrittenPost] = useState("");
+  const[container,setContainer]=useState(false);
   const [posts, setPosts] = useState([]);
 
   const toggleGifSearchBox = () => {
@@ -37,6 +38,7 @@ export function UserInput() {
     } else if (!selectedGifShow) {
       alert("select a gif");
     }
+    setContainer(true)
   };
 
   return (
@@ -75,36 +77,39 @@ export function UserInput() {
       <div className={style.post}>
         <div></div>
       </div>
-      <div className={style.post_container}>
-        {posts
-          ? posts.map((post) => (
-            <>
-              <div className={style.indiviual_container}>
-                <div className={style.posts} key={post.image}>
-                <div className={style.nav}>
-                  <img src={user} alt="user_img" />
-                  <p>Biswaranjan</p>
-                  <p>{`${post.date.getDate()} ${post.date.toLocaleString(
-                    "en-us",
-                    { month: "long" }
-                  )} at ${post.date.getHours()}:${post.date.getMinutes()}`}</p>
-                </div>
-                  <div className={style.post_area}>
-                    <p>{post.text}</p>
-                    <img src={post.image} />
+      {container ? (
+        <div className={style.post_container}>
+          {posts
+            ? posts.map((post) => (
+                <>
+                  <div className={style.indiviual_container}>
+                    <div className={style.posts} key={post.image}>
+                      <div className={style.nav}>
+                        <img src={user} alt="user_img" />
+                        <p>Biswaranjan</p>
+                        <p>{`${post.date.getDate()} ${post.date.toLocaleString(
+                          "en-us",
+                          { month: "long" }
+                        )} at ${post.date.getHours()}:${post.date.getMinutes()}`}</p>
+                      </div>
+                      <div className={style.post_area}>
+                        <p>{post.text}</p>
+                        <img src={post.image} />
+                      </div>
+                      <div className={style.buttons}>
+                        <button>Like</button>
+                        <button>Comment</button>
+                        <button>Share</button>
+                      </div>
+                    </div>
                   </div>
-                  <div className={style.buttons}>
-                  <button>Like</button>
-                  <button>Comment</button>
-                  <button>Share</button>
-                </div>
-                </div>
-                
-                </div>
-              </>
-            ))
-          : ""}
-      </div>
+                </>
+              ))
+            : ""}
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
